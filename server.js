a472edd1d60a368d084b3b7240fefa90288225f9
villalpando <villalpandooctavio@johndeere.com>
2021-03-05 18:54:52 -0600

    Testing url params

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+
+
 const app = express();
+
+const books = ["Lost Paradise", "Little Prince", "The Shining"]
 
 app.get('/', (req, res) => {
     res.send("Hi, loosers!")
+})
+
+app.get('/books/:index', (req, res) => {
+    res.send(books[req.params.index]);
 })
 
 app.listen(3000, () => {
