dd5f176b225bca9563c526957c6a11ea7c7285de
villalpando <villalpandooctavio@johndeere.com>
2021-03-05 20:17:52 -0600

    Configuring models folder and adding some testing data in books.js

--- server.js
+++ server.js
@@ -3,15 +3,27 @@ const express = require('express');
 
 const app = express();
 
-const books = ["Lost Paradise", "Little Prince", "The Shining"]
+const books = ["LostParadise", "Little Prince", "The Shining", "LostParadise"]
 
 app.get('/', (req, res) => {
     res.send("Hi, loosers!")
 })
 
+app.get('/books/count', (req, res) => {
+    let book = req.query.book
+
+    let count = books.filter((val) => {
+        return val === book
+    }).length;
+
+    res.send(`The number of available ${book}\s is ${count}`);
+})
+
 app.get('/books/:index', (req, res) => {
     res.send(books[req.params.index]);
 })
+
+
 
 app.listen(3000, () => {
     console.log("Bookbook backend up!");
