de78f41f3128600f46005c9abb99ca6aff42252b
villalpando <villalpandooctavio@johndeere.com>
2021-03-08 11:08:41 -0600

    Edit form working, full crud app completed

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const books =  require('./models/books');
+const methodOverride = require('method-override');
 
 const app = express();
 
 //Middleware
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 app.use((req, res, next) => {
     console.log("Running on all routes");
     next();
@@ -37,11 +39,33 @@ app.get('/books/:index', function(req, res){
     res.render('show.ejs', {
 
         book: books[req.params.index]
+
     });
 })
 
+app.delete('/books/:index', (req, res) => {
+    books.splice(req.params.index, 1);
+    res.redirect('/books');
+})
 
+app.get('/books/:index/edit', (req, res) => {
+    res.render('edit.ejs', {
+        
+        book: books[req.params.index],
+        index: req.params.index
 
+    });
+});
+
+app.put('/books/:index', (req, res) => {
+    if(req.res.isRead === 'on') {
+        req.body.isRead = true;
+    } else {
+        req.body.isRead = false;
+    }
+        books[req.params.index] = req.body;
+        res.redirect('/books');
+})
 
 app.listen(3000, () => {
     console.log("Bookbook backend up!");
