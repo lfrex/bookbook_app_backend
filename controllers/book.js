590c29418439196c6e478d87b46b499176c1b203
villalpando <villalpandooctavio@johndeere.com>
2021-03-11 12:50:21 -0600

    Adding functions in backend to send different results to frontnd

--- controllers/book.js
+++ controllers/book.js
@@ -23,7 +23,14 @@ const getAll = (req, res) => {
     })
 }
 
-
+// const show = (req, res) => {
+//     Book.findByPk(req.params.index)
+//     .then(book => {
+//         res.render('show.ejs', {
+//             book: book
+//         });
+//     })
+// };
 const getBookById = (req, res) => {
     let sort = 'DESC';
     if(req.query.sorted === 'asc')
@@ -42,14 +49,6 @@ const getBookById = (req, res) => {
     })
 }
 
-// const show = (req, res) => {
-//     Book.findByPk(req.params.index)
-//     .then(book => {
-//         res.render('show.ejs', {
-//             book: book
-//         });
-//     })
-// };
 
 // const renderNew = (req, res) => {
 //     res.render('new.ejs');
@@ -67,6 +66,35 @@ const getBookById = (req, res) => {
 //         res.redirect('/books');
 //     })  
 // } 
+const createBook = (req, res) => {
+    if(req.body.isRead === 'on') {
+        req.body.isRead = true;
+    } else {
+        req.body.isRead = false;
+    }
+   
+    Book.create(req.body)
+    .then(newBook => {
+        res.status(constants.SUCCESS).json(newBook)
+    })
+    .catch(err => {
+        res.status(constants.INTERNAL_SERVER_ERROR).send(`ERROR: ${err}`);
+    })
+}
+
+const getBooksByUser = (req, res) => {
+    Book.findAll({
+        where: {
+            userId: req.user.id
+        }
+    })
+    .then(allBooks => {
+        res.status(constants.SUCCESS).json(allBooks)
+    })
+    .catch(err => {
+        res.status(constants.INTERNAL_SERVER_ERROR).send(`ERROR: ${err}`);
+    })
+}
 
 // const renderEdit = (req, res) => {
 //     Book.findByPk(req.params.index)
@@ -93,12 +121,72 @@ const getBookById = (req, res) => {
 //     })     
 // }
 
+const editBook = (req, res) => {
+    if(req.body.isRead === 'on') {
+        req.body.isRead = true;
+    } else {
+        req.body.isRead = false;
+    }
+    Book.update(req.body, {
+        where: {
+            id: req.params.bookId
+        },
+        returning: true
+    })
+    .then(updatedBook => {
+        if(updatedBook[0] === 0){
+            res.status(constants.BAD_REQUEST).send('ERROR: Incorrect Book Id')
+        }else{
+            Book.findByPk(req.params.bookId, {
+                include: [
+                    {
+                        model: User,
+                        attributes: ['id', 'username']
+                    }
+                ]
+            })
+            .then(foundBook => {
+                if(foundBook === null){
+                    res.status(constants.BAD_REQUEST).send('ERROR: Incorrect Book Id')
+                }else{
+                    res.status(constants.SUCCESS).json(foundBook)
+                }
+            })
+            .catch(err => {
+                res.status(constants.INTERNAL_SERVER_ERROR).send(`ERROR: ${err}`);
+            })
+        }
+    })
+    .catch(err => {
+        res.status(constants.INTERNAL_SERVER_ERROR).send(`ERROR: ${err}`);
+    })
+}
+
 // const deleteBook = (req, res) => {
 // Book.destroy({ where: { id: req.params.index } })
 //     .then(() => {
 //         res.redirect('/books');
 //     })   
 // }
+const deleteBook = (req, res) => {
+    Book.findByPk(req.params.bookId)
+    .then(foundBook => {
+        if(foundBook.userId === req.user.id){
+            Book.destroy({
+                where: {id: req.params.bookId}
+            })
+            .then(() => {
+                res.status(constants.SUCCESS).send('success')
+            })
+        } else {
+            res.status(constants.FORBIDDEN).send('ERROR: Book is not owned by User')
+        }
+    })
+    .catch(err => {
+        res.status(constants.INTERNAL_SERVER_ERROR).send(`ERROR: ${err}`);
+    })
+}
+
 
 module.exports = {
     // index,  
@@ -108,6 +196,10 @@ module.exports = {
     // renderEdit,
     // editBook,
     // deleteBook,
+    getBooksByUser,
+    createBook,
     getAll,
-    getBookById
+    editBook,
+    getBookById,
+    deleteBook
 };
